4ddb58eea79504eb9a080fa9f619e0fb2f74963d
Sameer <spuri4096@gmail.com>
2017-10-05 16:03:30 -0500

    Just limit it...people walking through the door is throwing it off

--- photoresistor.ino
+++ photoresistor.ino
@@ -14,14 +14,11 @@ values gives higher readings) from pin 0 to GND. (see appendix of arduino notebo
 ----------------------------------------------------
 */
 
-int lightPin = 0;  //define a pin for Photo resistor
-
 void setup()
 {
     Serial.begin(600);  //Begin serial communcation
 }
 
-int lastRead = 0;
 bool isOpen = false;
 void loop()
 {
@@ -30,12 +27,10 @@ void loop()
     Serial.write(isOpen ? (byte) 1 : (byte) 0);
     Serial.flush();
   }
-   if ((analogRead(0) - lastRead) > 200 ) {
+   if (analogRead(0) > 200 ) {
     isOpen = true;
-    lastRead = analogRead(0);
-   } else if (lastRead - analogRead(0) > 200) {
+   } else {
     isOpen = false;
-    lastRead = analogRead(0);
    }
    delay(10); //short delay for faster response to light.
 }
